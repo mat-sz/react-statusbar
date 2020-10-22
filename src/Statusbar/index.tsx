5ff213609270b610cd7b12dc4b7978f12edf89e4
Mat Sz <contact@matsz.dev>
2020-10-22 18:17:46 +0200

    added a height property

--- src/Statusbar/index.tsx
+++ src/Statusbar/index.tsx
@@ -13,6 +13,7 @@ export interface StatusbarTheme {
 
 export interface StatusbarProps {
   theme?: 'dark' | 'light' | StatusbarTheme;
+  height?: string;
   className?: string;
   left?: ReactNode;
   right?: ReactNode;
@@ -21,31 +22,39 @@ export interface StatusbarProps {
 export const Statusbar: React.FC<StatusbarProps> = ({
   left,
   right,
+  height = '30px',
   className,
   theme = 'dark'
 }) => {
   const style = useMemo<any>(() => {
+    const styles: any = {
+      height
+    };
+
     switch (theme) {
       case 'dark':
         return {
+          ...styles,
           '--statusbar-primary-color': '#eee',
           '--statusbar-background-color': '#333',
           '--statusbar-font-family': defaultFontFamily
         };
       case 'light':
         return {
+          ...styles,
           '--statusbar-primary-color': '#111',
           '--statusbar-background-color': '#ddd',
           '--statusbar-font-family': defaultFontFamily
         };
       default:
         return {
+          ...styles,
           '--statusbar-primary-color': theme.primaryColor,
           '--statusbar-background-color': theme.backgroundColor,
           '--statusbar-font-family': theme.fontFamily ?? defaultFontFamily
         };
     }
-  }, [theme]);
+  }, [theme, height]);
 
   return (
     <div className={styles.statusbar + ' ' + (className ?? '')} style={style}>
