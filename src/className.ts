36505ae82f7a8a12fddc4b176c745e4420cc6958
Mat Sz <contact@matsz.dev>
2020-10-28 18:10:08 +0100

    added a className helper function

---
+++ src/className.ts
@@ -0,0 +1,24 @@
+export function className(
+  ...classNames: (string | Record<string, boolean> | undefined | null)[]
+) {
+  let result = '';
+
+  for (const item of classNames) {
+    if (!item) {
+      continue;
+    }
+
+    if (typeof item === 'string') {
+      result += item + ' ';
+      continue;
+    }
+
+    for (const key of Object.keys(item)) {
+      if (item[key] === true) {
+        result += key + ' ';
+      }
+    }
+  }
+
+  return result;
+}
